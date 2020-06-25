0ee207a831a74a80b2d8941200b2aec100b88ee9
edvisionapp <32239632+edvisionapp@users.noreply.github.com>
2020-06-25 07:51:04 -0400

    Added MVC components

--- DataGov_API-Intro/Startup.cs
+++ DataGov_API-Intro/Startup.cs
@@ -15,6 +15,7 @@ namespace DataGov_API_Intro
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddMvc();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,12 +23,21 @@ namespace DataGov_API_Intro
     {
       if (env.IsDevelopment())
       {
+        //app.UseBrowserLink();
         app.UseDeveloperExceptionPage();
       }
-
-      app.Run(async (context) =>
+      else
       {
-        await context.Response.WriteAsync("Hello World!");
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}");
       });
     }
   }
